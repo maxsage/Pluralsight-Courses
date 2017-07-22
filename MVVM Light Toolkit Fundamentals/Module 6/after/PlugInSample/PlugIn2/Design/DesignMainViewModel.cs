b13debb941ec3a0477ab58f7073721b71aa4d322
maxsage <max.sage@torbay.gov.uk>
2017-07-22 14:01:31 +0100

    Now up to expandable list - testing

---
+++ MVVM Light Toolkit Fundamentals/Module 6/after/PlugInSample/PlugIn2/Design/DesignMainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlugIn2.Design
+{
+    class DesignMainViewModel
+    {
+        public string Counter
+        {
+            get
+            {
+                return "We are in Design Mode";
+            }
+        }
+
+    }
+}
