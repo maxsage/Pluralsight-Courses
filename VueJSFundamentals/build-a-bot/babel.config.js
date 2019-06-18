08c7d76a58c917ea38fc493ccffb17351a5f1870
maxsage <maxsage@gmail.com>
2019-06-18 17:21:40 +0100

    Added build-a-bot and build-a-bot-server to Pluralsight-Courses instead of having them as standalone projects

---
+++ VueJSFundamentals/build-a-bot/babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  presets: [[
+    '@vue/app', { useBuiltIns: 'entry' }],
+  ],
+};
+// issue when adding props validation String Array etc.
+// https://github.com/vuejs/vue-cli/issues/3678
